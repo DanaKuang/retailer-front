56dfc06baf05cdb7230bb6a7b4ae4684e16e915e
deguo <deguo>
2017-08-14 15:48:37 +0800

    “修改”

--- js/goldLeaf.js
+++ js/goldLeaf.js
@@ -26,29 +26,30 @@ $(document).ready(function() {
 	});
 	var url=window.location.href;
 	console.log(url);
+	var winConfig=null;
 	$.ajax({
 		type: "post",
 		url: "/seller-web/wxauth/jsapisignature",
 		data:{url:url},
+		async:false,
 		timeout: 10000, //超时时间设置，单位毫秒
 		success: function(data) {
 			console.log(data);
-			
-			
+			winConfig = data;	
 		}
 	});
 //	扫一扫
-		wx.config({
+		
+			wx.config({
 				debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-				appId:  "", // 必填，公众号的唯一标识
-				timestamp:'' , // 必填，生成签名的时间戳
-				nonceStr: '', // 必填，生成签名的随机串
-				signature: '', // 必填，签名，见附录1
+				appId:  winConfig.appId, // 必填，公众号的唯一标识
+				timestamp:winConfig.timestamp , // 必填，生成签名的时间戳
+				nonceStr: winConfig.nonceStr, // 必填，生成签名的随机串
+				signature: winConfig.signature, // 必填，签名，见附录1
 				jsApiList: [
 					'scanQRCode'
 				] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
 			});
-
 			wx.ready(function() {
 				// config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
 				// 
