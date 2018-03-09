bd2d225958a35b5a1db729cd961570cea699cc67
hanzha <1256170322@qq.com>
2018-03-09 16:35:09 +0800

    FIX: 烟台零售户接入河南，扫一扫测试

--- src/views/YantaiScanCode.vue
+++ src/views/YantaiScanCode.vue
@@ -34,22 +34,6 @@
           scanType: ["qrCode", "barCode"], // 可以指定扫二维码还是一维码，默认二者都有
           success: function(res) {
             // 当needResult 为 1 时，扫码返回的结果
-          },
-          cancel: function(res) {
-            alert(1)
-            window.history.go(-1);
-          },
-          trigger: function(res) {
-            alert(2)
-            window.history.go(-1);
-          },
-          fail: function(res) {
-            alert(3)
-            window.history.go(-1);
-          },
-          complete: function(res) {
-//            alert(4)
-            window.history.go(-1);
           }
         })
       })
@@ -59,7 +43,7 @@
       window.addEventListener("popstate", function(e) {  //回调函数中实现需要的功能
         window.history.go(-1);
         wx.closeWindow();
-        alert("我监听到了浏览器的返回按钮事件啦");
+//        alert("我监听到了浏览器的返回按钮事件啦");
 //        location.href='你要跳转的链接';  //在这里指定其返回的地址
       }, false);
 
